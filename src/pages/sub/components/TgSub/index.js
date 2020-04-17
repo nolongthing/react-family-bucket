390e57495bef603fef345ceac70a0a4fe4cb6886
nolongthing <18317773771@163.com>
2020-04-17 14:37:07 +0800

    加入SVG图应用

--- src/pages/sub/components/TgSub/index.js
+++ src/pages/sub/components/TgSub/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import stores from '../../../../redux';
+import styles from './tg.module.scss';
+import { ReactComponent as Pay } from 'static/svgs/pay.svg';
 
 function TgSub(props) {
     const { getState } = stores;
@@ -13,6 +15,7 @@ function TgSub(props) {
             这是投顾工作室订阅项
             <button onClick={handleLogin}>点击跳转登陆</button>
             <div>显示登陆状态：{getState() ? '已登录' : '未登录'}</div>
+            <Pay className={styles.icon} />
         </div>
     )
 }
